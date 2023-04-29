4ef4098d46de8a2771fa55bce07e3305ae714397
Maaitrayo Das <dasmaaitrayo@gmail.com>
2023-04-29 10:33:19 +0530

    single file

---
+++ utils/helperFunc.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import numpy as np
+
+
+# frame width and height
+frame_wid = 640
+frame_hyt = 480
+
+def imageLoader(folder_path):
+    # Get a list of all items in the folder
+    items = os.listdir(folder_path)
+    print(f"[!] Found {len(items)} images [!]")
+
+    images_path_list = []
+    # Loop through each item in the folder
+    for image in items:
+        # Get the path of the item
+        item_path = os.path.join(folder_path, image)
+        images_path_list.append(item_path)
+
+    # Return a tuple containing the list of image paths and the list of image names
+    return (images_path_list, items)
+
+def imagePreprosessing(path):
+    img = cv2.imread(path)
+    img = cv2.resize(img, (frame_wid, frame_hyt))
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.blur(gray,(3,3))
+
+    img_log = (np.log(blur+1)/(np.log(1+np.max(blur))))*255
+    # Specify the data type
+    img_log = np.array(img_log,dtype=np.uint8)
+
+    bilateral = cv2.bilateralFilter(img_log, 5, 75, 75)
+
+    edges = cv2.Canny(bilateral,100,200)
+
+    kernel = np.ones((5,5),np.uint8)
+    closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    
+    return closing
+
+def featureDetection(processsed_image):
+    img = processsed_image
+    # Create feature detecting method
+    # sift = cv2.xfeatures2d.SIFT_create()
+    # surf = cv2.xfeatures2d.SURF_create()
+    orb = cv2.ORB_create(nfeatures=1500)
+
+    # Make featured Image
+    keypoints, descriptors = orb.detectAndCompute(img, None)
+    featuredImg = cv2.drawKeypoints(img, keypoints, None)
+
+    return featuredImg
+
+
+
