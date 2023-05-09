21dc2e23b1a4966e38e4e183498651b7a448d243
Maaitrayo Das <dasmaaitrayo@gmail.com>
2023-05-09 21:31:10 +0530

    folder name change

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from utils.yolo_segment import YOLOSegmentationSupport
 from utils.helperFunc import imageLoader, frame_hyt, frame_wid
 import sys
 
+def find_width(points):
+     print(points)
 
 def detectCracks(image_path_list, image_name_list):
     for img_path, image_name in zip(image_path_list, image_name_list):
@@ -13,9 +15,10 @@ def detectCracks(image_path_list, image_name_list):
         bboxes, classes, segmentations, scores = ys.detect(img)
         for bbox, class_id, seg, score in zip(bboxes, classes, segmentations, scores):
             # print("bbox:", bbox, "class id:", class_id, "seg:", seg, "score:", score)
-            print("class id:", class_id)
+            # print("class id:", class_id)
             (x, y, x2, y2) = bbox
             if class_id == 0:
+                find_width(seg)
                 cv2.polylines(img, [seg], True, (0, 0, 255), 4)
                 # cv2.rectangle(img, (x, y), (x2, y2), (255, 0, 0), 2)
                 # cv2.putText(img, str(class_id), (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
